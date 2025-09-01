c0b7ce6517659c1b85768e79f4e5b31089d5fa4e
mahmed03 <mahmoud.ahmed@icr.ac.uk>
2025-09-01 08:43:44 +0100

    plot different tools separately

--- test-submit.sh
+++ test-submit.sh
@@ -23,7 +23,7 @@ module load Nextflow
 nextflow run ../main.nf \
     --output_dir ./results/ \
     -params-file ../test-params.json \
-    -profile cluster,test \
+    -profile local,test \
     -resume
 
 # usage: nextflow run [ local_dir/main.nf | git_url ]  
